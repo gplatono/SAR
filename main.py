d298456a292ed393d9601acf3c753358c9694919
gplatono <gr.plat@gmail.com>
2018-06-16 20:37:25 -0400

    1

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ class Agent(object):
         self.units = units
         self.perception_units = {}
         self.active_units = []
+        self.receptive_field = []
         self.firing_history = []
+        self.id_counter = len(units)
 
     def start(self, inputs):
         for input in inputs:
@@ -39,8 +41,36 @@ class Agent(object):
             sensory = self.perception_units[percept]
             if sensory not in self.active_units:
                 active_units += [sensory]
+                receptive_field += [sensory]
             sensory.fire()
+            
+    def create_association(self, units):
+        candidate = Set()
+        for u in units:
+            
+            
+        
+    def instantiate_unit(self, units):
+        if units not in self.units:
+            new_unit = Unit(self.id_counter, None, unit_type="default", content=units)
+            self.id_counter += 1
+            self.units += [new_unit]
+        self.units[units].fire()
 
+    def propagate_activations(self):
+        for i in range(0, len(self.receptive_field)-1):
+            instantiate_unit([self.receptive_field[i], self.receptive_field[i+1]])
+            if i < len(self.receptive_field) - 2:
+                instantiate_unit([self.receptive_field[i], self.receptive_field[i+1], self.receptive_field[i+2]])
+            if i < len(self.receptive_field) - 3:
+                instantiate_unit([self.receptive_field[i], self.receptive_field[i+1], self.receptive_field[i+2], self.receptive_field[i+3]])
+
+            
+    def cognitive_loop(self, input):
+        process_input(input)
+        
+        
+            
     def load(self):
         return 1
         
